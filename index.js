e45be9b03e97b30047ebde50d29bfe0cd132505e
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-08-10 17:33:21 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names){
+    let output = [];
+    for (let name of names){
+        output.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return output;
+}
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n);
+        n--
+    }
+}
